3da9aa382e0e6815caedde7d43f5a5659b5f9f0a
Lexa <lexaraea@gmail.com>
2019-06-17 22:23:55 -0400

    adding stores

---
+++ store_item.rb
@@ -0,0 +1,7 @@
+store = [
+  { "fruit" => "apple", "color" => "red", "price" => 1 },
+  { "fruit" => "banana", "color" => "yellow", "price" => 2},
+  { "fruit" => "pear", "color" => "green", "price" => 3}
+]
+
+p store[0]
