0c0fbdfc6771cea4267b30b4e6482c7415fa93cc
Lexa <lexaraea@gmail.com>
2019-06-18 20:04:53 -0400

    add class and medthods to store

--- store_item.rb
+++ store_item.rb
@@ -1,7 +1,37 @@
 store = [
-  { "fruit" => "apple", "color" => "red", "price" => 1 },
-  { "fruit" => "banana", "color" => "yellow", "price" => 2},
-  { "fruit" => "pear", "color" => "green", "price" => 3}
+  { "type" => "apple", "color" => "red", "price" => 1 },
+  { "type" => "banana", "color" => "yellow", "price" => 2},
+  { "type" => "pear", "color" => "green", "price" => 3}
 ]
 
-p store[0]
+class Store
+  def initialize(type, color, price)
+    @type = type
+    @color = color
+    @price = color
+  end
+
+# reader/getter
+  def type
+    @type
+  end
+
+# reader/getter
+  def color
+    @color
+  end
+
+# writer/setter
+  def price=(price)
+    @price = price
+  end
+
+  def print_info
+    p "The #{@type} is #{@color} and costs #{@price}"
+  end
+end
+
+
+store1 = Store.new("apple", "red", 1)
+
+store1.print_info
