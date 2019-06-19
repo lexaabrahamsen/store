cae0939dbf96c76b9f2a752bc9f5110dc58298c1
Lexa <lexaraea@gmail.com>
2019-06-19 16:45:05 -0400

    Work through DRY'ing up example

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -1,7 +1,7 @@
-class Car
-  def initialize(input_speed, input_direction)
-    @speed = input_speed
-    @direction = input_direction
+class Vehicle
+  def initialize
+    @speed = 0
+    @direction = 'north'
   end
 
   def brake
@@ -9,44 +9,30 @@ class Car
   end
 
   def accelerate
-
-    @speed += 10
+    @direction += 10
   end
 
   def turn(new_direction)
     @direction = new_direction
   end
+end
 
+class Car < Vehicle
   def honk_horn
     puts "Beeeeeeep!"
   end
 end
 
-class Bike
-  def initialize(input_speed, input_direction)
-    @speed = input_speed
-    @direction = input_direction
-  end
-
-  def brake
-    @speed = 0
-  end
-
-  def accelerate
-    @speed += 10
-  end
-
-  def turn(new_direction)
-    @direction = new_direction
-  end
-
+class Bike < Vehicle
   def ring_bell
     puts "Ring ring!"
   end
 end
 
-bike = Bike.new(0, "North")
-car = Car.new(0, "North")
+
+bike = Bike.new
+car = Car.new
+
 
 bike.ring_bell
 car.honk_horn
